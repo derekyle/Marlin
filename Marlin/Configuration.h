7188226113962cbc13d1242a046e13dabb8d6ccc
Derek <dc@dcarr.net>
2021-04-17 23:52:26 -0700

    new custom machine name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 V2 427 2"
+#define CUSTOM_MACHINE_NAME "Ender-3 427 BLTR"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
